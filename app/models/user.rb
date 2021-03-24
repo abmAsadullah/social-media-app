840aba2c04a6779b9078e749b7e611cd00cfdb0e
A B M Asadullah <abmasadullah@gmail.com>
2021-03-24 21:48:00 +0600

    Add friendship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,37 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 20 }
 
+  has_many :friendships
+  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+
+  def friends
+    friends_array = friendships.map{|friendship| friendship.friend if friendship.request}
+    friends_array + inverse_friendships.map{|friendship| friendship.user if friendship.request}
+    friends_array.compact
+  end
+
+    # Users who have yet to confirme friend requests
+    def pending_friends
+      friendships.map{|friendship| friendship.friend if !friendship.request}.compact
+    end
+  
+    # Users who have requested to be friends
+    def friend_requests
+      inverse_friendships.map{|friendship| friendship.user if !friendship.request}.compact
+    end
+  
+    def confirm_friend(user)
+      friendship = inverse_friendships.find{|friendship| friendship.user == user}
+      friendship.request = true
+      friendship.save
+    end
+  
+    def friend?(user)
+      friends.include?(user)
+    end
 end
