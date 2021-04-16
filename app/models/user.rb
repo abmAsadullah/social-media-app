1191836798bead248296dd4007ecfec2be68691c
A B M Asadullah <abmasadullah@gmail.com>
2021-04-16 21:24:29 +0600

    Linter fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,6 @@ class User < ApplicationRecord
 
   has_many :friendships
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
-
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
@@ -17,14 +16,14 @@ class User < ApplicationRecord
     friends_i_sent_invitation = Invitation.where(user_id: id, confirmed: true).pluck(:friend_id)
     friends_i_got_invitation = Invitation.where(friend_id: id, confirmed: true).pluck(:user_id)
     ids = friends_i_sent_invitation + friends_i_got_invitation
-    User.where(id:ids)
+    User.where(id: ids)
   end
 
   def friend_with(user)
-      Invitation.confirmed_record?(id, user.id)
+    Invitation.confirmed_record?(id, user.id)
   end
 
   def send_invitation(user)
-      invitations.create(friend_id: user.id)
+    invitations.create(friend_id: user.id)
   end
 end
