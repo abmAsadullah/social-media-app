eae3d805f777a3072a257089d1358229e750927f
A B M Asadullah <abmasadullah@gmail.com>
2021-05-05 11:20:52 +0600

    Fix linters

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -11,7 +11,9 @@ module UserHelper
 
   def index_friendship(user)
     if user.pending_friends.include?(current_user)
+      # rubocop:disable LineLength
       render partial: 'accept_form', locals: { friendreq: user.friendships.where(friend_id: current_user.id).ids.split('/').join(',') }
+      # rubocop:enable LineLength
     elsif user.friends.include?(current_user)
       label_tag 'Friends'
     else
@@ -23,7 +25,9 @@ module UserHelper
     if current_user.pending_friends.include?(user)
       label_tag 'Pending'
     elsif user.friend_requests.include?(current_user)
+      # rubocop:disable LineLength
       render partial: 'accept_form', locals: { friendreq: user.friendships.where(friend_id: current_user.id).ids.split('/').join(',') }
+      # rubocop:enable LineLength
     elsif !user.friends.include?(current_user)
       button_to 'Add Friend', friendships_path(user_id: current_user.id, friend_id: user.id)
     else
